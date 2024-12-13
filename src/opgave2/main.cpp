862db3f54818796500b1635cade2102f20b05401
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-12-13 13:17:40 +0100

    Fixed file structure

--- src/opgave2/main.cpp
+++ src/opgave2/main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
-#include <cstdlib>
-
-
-void findMax(int *p_array, size_t size, int *result) {
-  if( size <= 0 ) {
-    *result = 0;
-  }
-
-  *result = p_array[0];
-  for(int i = 1; i < size; i++) {
-    if(p_array[i] > *result) {
-      *result = p_array[i];
-    }
-  }
-}
+#include "max.h"
 
 int main(int argc, char** argv) {
   int arr[10] = {-15, 7, -3, 10, -20, 5, 19, -8, 13, -2};
   int result;
+
   findMax(arr, sizeof(arr)/sizeof(int), &result);
   std::cout << result << std::endl;
   
