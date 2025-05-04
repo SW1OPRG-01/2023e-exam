3750b3fe58933f50550290fe2af06dd9ef9bad03
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2025-05-04 15:29:45 +0200

    Clean up

--- src/opgave1/capacitance.cpp
+++
@@ -1,5 +0,0 @@
-#include "capacitance.h"
-
-double capacitance(double epsilon, double area,double distance) {
-  return (epsilon * area) / distance;
-}
--- src/opgave2/max.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAX_H
-#define MAX_H
-
-#include <cstdlib>
-
-void findMax(int *p_array, size_t size, int *result);
-
-#endif
